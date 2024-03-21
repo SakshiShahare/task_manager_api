44f676dddf0ed71ec86af078a468a9b8e0524e59
Sakshi <sakshi@Sakshis-MacBook-Pro.local>
2024-03-21 14:09:38 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,22 @@
+require("dotenv").config();
+const express = require("express");
+const {connectDB} = require("./connect/connect.js");
+
+const app = express();
+
+const port = 3000;
+
+
+const save = async () =>{
+    try {
+        await connectDB();
+        console.log("connected successfully");
+        app.listen(port , () =>{
+            console.log("listening on server 3000");
+        })
+    } catch (error) {
+        console.log("connection error");
+    }
+}
+
+save();
